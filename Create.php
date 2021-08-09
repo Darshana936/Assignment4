d07793defb4527945dcfe94ab4534afa9c2152e1
Darshana936 <jethavadarshana@gmail.com>
2021-08-09 06:07:20 -0700

    Initial commit

---
+++ Create.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Operation</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   
+       <div class="AddHeader">
+           <h1 class="AddHead">Create Category</h1>
+       </div>
+<div class="AddContainer">
+       <form action="" method="post">
+           <div class="form-group">
+               <label for="InputCategory">Category Name</label><br>
+               <input type="text" name="category" class="form-control" id="InputCategory">
+           </div><hr>
+           <button type="submit" class="btn" name="submit">Submit</button>
+       </form>
+   </div>
+    </div>
+</body>
+</html>
